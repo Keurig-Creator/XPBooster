34d8078a4edda7ff517a1d9c9fc74f7e873d8a37
David <threetwoone112@gmail.com>
2024-04-02 00:30:31 -0500

    Update XPBoostCommand.java

--- src/java/com/keurig/xpbooster/command/XPBoostCommand.java
+++ src/java/com/keurig/xpbooster/command/XPBoostCommand.java
@@ -15,7 +15,8 @@ import org.bukkit.entity.Player;
 
 import java.util.Calendar;
 
-@CommandAlias("xpboost|expboost|exp|ex")
+@CommandAlias("xpbooster|xpboost|expboost|exp|ex")
+@CommandPermission("xpbooster.admin")
 public class XPBoostCommand extends BaseCommand {
 
     @Dependency
@@ -26,7 +27,7 @@ public class XPBoostCommand extends BaseCommand {
         Chat.message(sender,
                 "&8&l----- &6&lXPBooster &e&lCommands &8&l-----\n" +
                         " \n" + // empty Space
-                        "&e/xpbooster &6info &7[player]" +
+                        "&e/xpbooster &6info &7[player]\n" +
                         "&e/xpbooster &6set &7<player> <multiplier> &8[time]\n" +
                         "&e/xpbooster &6add &7<player> &8[time]\n" +
                         "&e/xpbooster &6remove &7<player>\n" +
@@ -44,9 +45,23 @@ public class XPBoostCommand extends BaseCommand {
     public void onSet(CommandSender sender, OfflinePlayer toSet, String m, String[] args) {
         Replacement replace = Replacement.createReplacement(getName(), sender.getName(), toSet.getName());
 
-        Double multiplier = validateMultiplier(sender, m, replace);
-        if (multiplier == null) return;
+        if (!NumUtil.isNumber(m, false)) {
+            Chat.message(sender, Language.INVALID_NUMBER.toString());
+            return;
+        }
 
+        double multiplier = Double.parseDouble(m);
+
+        if (multiplier <= plugin.config.getDouble("minimum_multiplier")) {
+            Chat.message(sender, Language.MINIMUM_MULTIPLIER.toString(replace));
+            return;
+        } else if (multiplier > plugin.config.getDouble("maximum_multiplier")) {
+            Chat.message(sender, Language.MAXIMUM_MULTIPLIER.toString(replace));
+            return;
+        } else if (!plugin.config.getBoolean("full_range_multiplier") && !NumUtil.isWholeOrHalf(multiplier)) {
+            Chat.message(sender, Language.FULL_RANGE_MULTIPLIER.toString(replace));
+            return;
+        }
         replace.addReplacement(Replacement.DURATION_REGEX, "PERMANENT");
         replace.addReplacement(Replacement.MULTIPLIER_REGEX, String.valueOf(multiplier));
 
@@ -66,8 +81,6 @@ public class XPBoostCommand extends BaseCommand {
 
             calendar.add(calenderN, extractNumber);
             time = calendar.getTime().getTime();
-
-            Chat.log(NumUtil.convertDateToStr(date));
             replace.addReplacement(Replacement.DURATION_REGEX, NumUtil.convertDateToStr(date));
         }
 
@@ -77,9 +90,47 @@ public class XPBoostCommand extends BaseCommand {
                 .multiplier(multiplier)
                 .date(time).build();
 
-        plugin.getBoostHandler().addBoost(toSet.getPlayer().getUniqueId(), expBoost);
+        plugin.getBoostHandler().addBoost(toSet.getUniqueId(), expBoost);
 
         sender.sendMessage(Language.SET_XPBOOST_MESSAGE.toString(replace));
+
+    }
+
+    @Subcommand("add")
+    @Syntax("<player> [time]")
+    @CommandCompletion("@players")
+    public void onAdd(CommandSender sender, OfflinePlayer toSet, String timeDate) {
+        Replacement replace = Replacement.createReplacement(getName(), sender.getName(), toSet.getName());
+        replace.addReplacement(Replacement.DURATION_REGEX, "PERMANENT");
+
+        XPBoostHandler boostHandler = plugin.getBoostHandler();
+
+        if (!boostHandler.hasBoost(toSet.getUniqueId())) {
+            sender.sendMessage(Language.PLAYER_NO_BOOST.toString(replace));
+            return;
+        }
+
+        EXPBoost boost = boostHandler.getBoost(toSet.getUniqueId());
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(boost.getDate());
+
+
+        int extractNumber = NumUtil.extractNumber(timeDate);
+        int calenderN = NumUtil.convertToCalendar(timeDate);
+
+        if (extractNumber == -1 || calenderN == -1) {
+            Chat.message(sender, Language.INVALID_DATE.toString(replace));
+            return;
+        }
+
+        calendar.add(calenderN, extractNumber);
+        replace.addReplacement(Replacement.DURATION_REGEX, NumUtil.convertDateToStr(timeDate));
+
+        boost.setDate(calendar.getTime().getTime());
+        plugin.getDataConfig().save();
+
+        sender.sendMessage(Language.ADD_XPBOOST_MESSAGE.toString(replace));
 
     }
 
@@ -105,9 +156,10 @@ public class XPBoostCommand extends BaseCommand {
         Replacement replace = Replacement.createReplacement(getName(), player.getName());
 
         if (plugin.getBoostHandler().hasBoost(player.getUniqueId())) {
-            Chat.message(player, "&aYou have an active EXP BOOSTER!");
+            EXPBoost boost = plugin.getBoostHandler().getBoost(player.getUniqueId());
+            Chat.message(player, "&8&l----- &eActive &6Booster Info &8&l-----\n \n&7Multiplier &e" + boost.getMultiplier() + "\n&7Time remaining: &e" + boost.getRemainingTime() + "\n ");
         } else {
-            Chat.message(player, "&cYou have no active boosters");
+            Chat.message(player, Language.PLAYER_NO_BOOST.toString());
         }
     }
 
@@ -118,31 +170,10 @@ public class XPBoostCommand extends BaseCommand {
 
         if (plugin.getBoostHandler().hasBoost(target.getUniqueId())) {
             EXPBoost boost = plugin.getBoostHandler().getBoost(target.getUniqueId());
-            Chat.message(sender, "&aTarget has an active booster: Multiplier: " + boost.getMultiplier() + ", Remaining Time: " + boost.getRemainingTime());
+            Chat.message(sender, "&8&l----- &eActive &6Booster Info &8&l-----\n \n&7Player &e" + target.getName() + "\n&7Multiplier &e" + boost.getMultiplier() + "\n&7Time remaining: &e" + boost.getRemainingTime() + "\n ");
         } else {
-            Chat.message(sender, "&cTarget has no active boosters");
+            Chat.message(sender, Language.TARGET_NO_BOOST.toString());
         }
-    }
-
-    private Double validateMultiplier(CommandSender sender, String m, Replacement replace) {
-        if (!NumUtil.isNumber(m, false)) {
-            Chat.message(sender, Language.INVALID_NUMBER.toString());
-            return null;
-        }
-
-        double multiplier = Double.parseDouble(m);
-
-        if (multiplier <= plugin.config.getDouble("minimum_multiplier")) {
-            Chat.message(sender, Language.MINIMUM_MULTIPLIER.toString(replace));
-            return null;
-        } else if (multiplier > plugin.config.getDouble("maximum_multiplier")) {
-            Chat.message(sender, Language.MAXIMUM_MULTIPLIER.toString(replace));
-            return null;
-        } else if (!plugin.config.getBoolean("full_range_multiplier") && !NumUtil.isWholeOrHalf(multiplier)) {
-            Chat.message(sender, Language.FULL_RANGE_MULTIPLIER.toString(replace));
-            return null;
-        }
-        return multiplier;
     }
 
 }
