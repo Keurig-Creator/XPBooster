8398def28188d1fb955b6f9c907dfdc3e7bb1796
Keurig <64445183+Keurig-Creator@users.noreply.github.com>
2024-04-04 10:59:03 -0500

    added a store option

--- src/java/com/keurig/xpbooster/base/menu/PlayerMenu.java
+++ src/java/com/keurig/xpbooster/base/menu/PlayerMenu.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 import org.bukkit.entity.Player;
 
+import java.util.HashMap;
 import java.util.Stack;
 
 
@@ -13,8 +14,32 @@ import java.util.Stack;
 @AllArgsConstructor
 public class PlayerMenu {
 
+    private final HashMap<String, Object> store = new HashMap<>();
     private final Stack<Menu> history = new Stack<>();
     private final Player player;
+
+    public String getString(String key) {
+        return getData(key, String.class);
+    }
+
+    public void setString(String key, String value) {
+        store.put(key, value);
+    }
+
+
+    public void setData(String key, Object obj) {
+        store.put(key, obj);
+    }
+
+    public <T> T getData(String key, Class<T> ref) {
+        Object obj = store.get(key);
+
+        if (obj != null) {
+            return ref.cast(obj);
+        }
+
+        return null;
+    }
 
     public void addHistory(Menu menu) {
         history.push(menu);
