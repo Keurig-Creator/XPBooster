452ea5b473c895dcacda2129fbd9fae9f1876639
David <threetwoone112@gmail.com>
2024-04-03 21:56:53 -0500

    Update InventoryMoveListener.java

--- src/java/com/keurig/xpbooster/event/InventoryMoveListener.java
+++ src/java/com/keurig/xpbooster/event/InventoryMoveListener.java
@@ -2,18 +2,17 @@ package com.keurig.xpbooster.event;
 
 import com.keurig.xpbooster.XPBoostPlugin;
 import com.keurig.xpbooster.api.XPBoostAPI;
-import com.keurig.xpbooster.base.EXPBoost;
 import com.keurig.xpbooster.base.Voucher;
 import com.keurig.xpbooster.base.VoucherManager;
 import com.keurig.xpbooster.base.shop.ShopMenu;
-import com.keurig.xpbooster.util.NumUtil;
+import com.keurig.xpbooster.language.Language;
+import com.keurig.xpbooster.util.Chat;
 import lombok.AllArgsConstructor;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
-
-import java.util.Calendar;
 
 @AllArgsConstructor
 public class InventoryMoveListener implements Listener {
@@ -40,23 +39,26 @@ public class InventoryMoveListener implements Listener {
             if (voucher != null) {
                 if (shop.isInstantClaim(voucher)) {
 
-                    Calendar calendar = Calendar.getInstance();
-
-                    int extractNumber = NumUtil.extractNumber(voucher.getTime());
-                    int calenderN = NumUtil.convertToCalendar(voucher.getTime());
-
-                    calendar.add(calenderN, extractNumber);
-
-                    EXPBoost expBoost = EXPBoost.builder()
-                            .uuid(player.getUniqueId())
-                            .multiplier(voucher.getMultiplier())
-                            .date(calendar.getTime().getTime()).build();
-
-                    XPBoostAPI.addBoost(player.getUniqueId(), expBoost);
+                    if (XPBoostAPI.hasBoost(player.getUniqueId())) {
+                        if (XPBoostAPI.getBoost(player.getUniqueId()).getMultiplier() != voucher.getMultiplier()) {
+                            if (!event.isShiftClick()) {
+                                player.playSound(player, Sound.BLOCK_NOTE_BLOCK_PLING, 1, 1);
+                                Chat.message(player, "&cYou already have a different type XP boost. To override and use new one hold shift and left click.");
+                            } else {
+                                XPBoostAPI.removeBoost(player.getUniqueId());
+                            }
+                        }
+                        XPBoostAPI.addBoost(player.getUniqueId(), voucher);
+                    } else {
+                        XPBoostAPI.addBoost(player.getUniqueId(), voucher);
+                    }
+                    player.closeInventory();
                 } else {
                     player.getInventory().addItem(voucher.getItem());
                     player.closeInventory();
                 }
+
+                player.sendMessage(Language.ADD_XPBOOST_MESSAGE.toString());
             }
 
             event.setCancelled(true);
