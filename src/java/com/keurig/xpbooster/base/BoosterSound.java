a84a564a356f8f19637da4957f1c7a6d49105dad
David <threetwoone112@gmail.com>
2024-04-07 18:15:31 -0500

    Create BoosterSound.java

---
+++ src/java/com/keurig/xpbooster/base/BoosterSound.java
@@ -0,0 +1,13 @@
+package com.keurig.xpbooster.base;
+
+import lombok.AllArgsConstructor;
+import org.bukkit.Sound;
+
+@AllArgsConstructor
+public class BoosterSound {
+
+    public Sound sound;
+    public float volume;
+    public float pitch;
+
+}
