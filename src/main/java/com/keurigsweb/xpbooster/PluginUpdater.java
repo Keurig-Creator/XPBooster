23fbba7c2a28b8c9538bc4dc1f6a0a86b1df92bb
David <threetwoone112@gmail.com>
2024-07-18 01:11:38 -0500

    Create PluginUpdater.java

---
+++ src/main/java/com/keurigsweb/xpbooster/PluginUpdater.java
@@ -0,0 +1,56 @@
+package com.keurigsweb.xpbooster;
+
+import com.keurigsweb.xpbooster.util.Chat;
+import lombok.Getter;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class PluginUpdater {
+
+    private JavaPlugin plugin;
+    private int resourceId; // Spigot resource ID
+    @Getter
+    private static boolean update;
+    public static String CURRENT_VERSION;
+    public static String LATEST_VERSION;
+
+    public PluginUpdater(JavaPlugin plugin, int resourceId) {
+        this.plugin = plugin;
+        this.resourceId = resourceId;
+    }
+
+    public void checkForUpdates() {
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                Chat.log("test");
+                try {
+                    HttpURLConnection connection = (HttpURLConnection) new URL("https://api.spigotmc.org/legacy/update.php?resource=" + resourceId).openConnection();
+                    connection.setRequestMethod("GET");
+                    String latestVersion = new BufferedReader(new InputStreamReader(connection.getInputStream())).readLine();
+
+                    // Compare versions
+                    String currentVersion = plugin.getDescription().getVersion();
+
+                    Chat.log(currentVersion);
+                    Chat.log(latestVersion);
+                    if (!latestVersion.equals(currentVersion)) {
+                        Chat.log("A new version is available: " + latestVersion);
+                        update = true;
+                        // Notify players or perform update actions here
+                    }
+                    LATEST_VERSION = latestVersion;
+                    CURRENT_VERSION = currentVersion;
+                } catch (IOException ignored) {
+                }
+            }
+        }.runTaskAsynchronously(plugin);
+    }
+
+}
