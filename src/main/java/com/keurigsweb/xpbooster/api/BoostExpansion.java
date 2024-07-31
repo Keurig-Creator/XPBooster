ae1e35ee0a32865ebd0459714966719da6b3e6e3
David <threetwoone112@gmail.com>
2024-07-30 23:51:40 -0500

    rework of papi

--- src/main/java/com/keurigsweb/xpbooster/api/BoostExpansion.java
+++ src/main/java/com/keurigsweb/xpbooster/api/BoostExpansion.java
@@ -25,11 +25,25 @@ public class BoostExpansion extends PlaceholderExpansion {
     @Override
     public @Nullable String onRequest(OfflinePlayer player, @NotNull String params) {
         if (params.equalsIgnoreCase("boost")) {
+            return player == null ? null : XPBoostAPI.getMultiplier(player.getPlayer()) + "";
+        } else if(params.equalsIgnoreCase("boostformatted")) {
             return player == null ? null : NumUtil.formatMultiplier(XPBoostAPI.getMultiplier(player.getPlayer()));
         } else if(params.equalsIgnoreCase("total")) {
+            return player == null ? null : XPBoostAPI.getTotalBoost(player.getPlayer()) + "";
+        } else if(params.equalsIgnoreCase("totalformatted")) {
             return player == null ? null : NumUtil.formatMultiplier(XPBoostAPI.getTotalBoost(player.getPlayer()));
         } else if (params.equalsIgnoreCase("time")) {
-            return player == null ? null : XPBoostAPI.getRemainingTime(player.getPlayer());
+            return player == null ? null : XPBoostAPI.getBoost(player.getUniqueId()).getRemainingTime();
+        } else if (params.equalsIgnoreCase("timeformatted")) {
+            return player == null ? null : XPBoostAPI.getBoost(player.getUniqueId()).getRemainingTimeFormat();
+        } else if (params.equalsIgnoreCase("global")) {
+            return XPBoostAPI.getGlobalMultiplier() + "";
+        } else if (params.equalsIgnoreCase("globalformatted")) {
+            return NumUtil.formatMultiplier(XPBoostAPI.getGlobalMultiplier());
+        } else if (params.equalsIgnoreCase("globaltime")) {
+            return XPBoostAPI.getGlobalMultiplierTime();
+        } else if (params.equalsIgnoreCase("globaltimeformatted")) {
+            return XPBoostAPI.getGlobalMultiplierTimeFormat();
         }
 
         return null;
